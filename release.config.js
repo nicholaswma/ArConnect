e9e74326bb21896e8b21b37960c895e507301f10
nicholas ma <nicholas.maa@gmail.com>
2024-01-24 16:30:55 -0800

    fix: something

--- release.config.js
+++ release.config.js
@@ -12,7 +12,7 @@ const config = {
     [
       "@semantic-release/git",
       {
-        assets: ["package.json"], // Ensure package.json is included as an asset
+        assets: ["package.json"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }
